ebedbcb2afc1be4f14fb3be5507565734b14b372
ChoiSungWon <spancho0810@naver.com>
2026-02-11 10:28:55 +0900

    feat: policyDetail 상단 헤더 로그인 상태에 따라 버튼 수정

--- src/main/java/org/sungsung/youthpolicy/controller/PolicyController.java
+++ src/main/java/org/sungsung/youthpolicy/controller/PolicyController.java
@@ -1,5 +1,6 @@
 package org.sungsung.youthpolicy.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -7,7 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.sungsung.youthpolicy.domain.vo.member.MemberVO;
+import org.sungsung.youthpolicy.service.member.MemberService;
 import org.sungsung.youthpolicy.service.policy.PolicyService;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/policy/*")
@@ -16,9 +21,11 @@ import org.sungsung.youthpolicy.service.policy.PolicyService;
 public class PolicyController {
 
     private final PolicyService policyService;
-
+    private final MemberService memberService;
     @GetMapping("/detail/{policyId}")
-    public String policyDetailPage(@PathVariable("policyId")String policyId, Model model){
+    public String policyDetailPage(@PathVariable("policyId")String policyId, Model model, HttpSession session){
+        Optional<MemberVO> loginMember = memberService.findMemberById((Long)session.getAttribute("sessionId"));
+        loginMember.ifPresent(member -> model.addAttribute("loginMember", member));
         model.addAttribute("policy", policyService.policyDetail(policyId));
         return "policy/policyDetail";
     }
