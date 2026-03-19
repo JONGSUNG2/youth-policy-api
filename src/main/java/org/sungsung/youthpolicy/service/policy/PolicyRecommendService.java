d7db79626baf357cf36f7dab853d0dcbf789921e
ChoiSungWon <spancho0810@naver.com>
2026-03-19 14:54:20 +0900

    feature:테이블 불필요 컬럼 해쉬값 제거 및 수정

--- src/main/java/org/sungsung/youthpolicy/service/policy/PolicyRecommendService.java
+++ src/main/java/org/sungsung/youthpolicy/service/policy/PolicyRecommendService.java
@@ -23,7 +23,7 @@ public class PolicyRecommendService {
 
         // 1. 조건 조회
         PolicyConditionVO condition = policyService.findRecommendPolicyByHash(hash);
-
+        log.info(condition.toString());
         // 2. 필터링된 정책 ID 리스트 조회
         List<String> policyIds = policyService.findFilteringPolicyIds(condition);
         if (policyIds.isEmpty()) {
@@ -42,7 +42,7 @@ public class PolicyRecommendService {
         // 5. DB에 추천 결과 저장
         for (PolicyRecommendVO recommendPolicy : recommendList) {
             recommendPolicy.setLoginId(memberLoginId);
-            recommendPolicy.setConditionHash(hash);
+            recommendPolicy.setPolicyConditionId(condition.getId());
             policyService.writeRecommendPolicy(recommendPolicy);
         }
         return true;
--- src/main/java/org/sungsung/youthpolicy/service/policy/PolicyServiceImpl.java
+++ src/main/java/org/sungsung/youthpolicy/service/policy/PolicyServiceImpl.java
@@ -97,5 +97,4 @@ public class PolicyServiceImpl implements PolicyService {
         return policyDAO.selectRecommendPolicyList(hash);
     }
 
-
 }
